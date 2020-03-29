6db244188c765cc1d0fc9b7e03c6bbc8891f6035
Armen Grigorian <info@propertystart.com.au>
2020-03-30 09:20:21 +1100

    Version 1

---
+++ data.js
@@ -0,0 +1,125 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.84347191593334,
+        "pitch": 0.007261601103987658,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.47357029407002926,
+          "pitch": 0.13837191229449353,
+          "rotation": 5.497787143782138,
+          "target": "2-master-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living",
+      "name": "Living",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.1073316505515045,
+        "pitch": 0.19278202820010293,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1197982880657502,
+          "pitch": 0.16273807575333166,
+          "rotation": 0,
+          "target": "2-master-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-master-bedroom",
+      "name": "Master Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.4131848331145944,
+        "pitch": 0.0425651368316835,
+        "fov": 1.3219339076578436
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1333215895864654,
+          "pitch": 0.28602234291854245,
+          "rotation": 0.7853981633974483,
+          "target": "1-living"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Unit 7, 2 Monash Green Drive, Clayton",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
